cd7c1c43543a3ed64206a245a608eecd3ca569b4
marky376 <mmutuamark@gmail.com>
2024-04-19 13:26:52 +0300

    A commit on some file to work on

--- Flask-blog/hello.py
+++ Flask-blog/hello.py
@@ -10,6 +10,10 @@ def index():
 def about():
     return '<h1>About Page!</h1>'
 
+@app.route('/whoami')
+def whoami():
+    return '<h1> My Name is Mark Munyao Mutua Son of Gregory Mutua Munyao
+    and Ceciia Mueni Mutisya. Brother to Chelsea Ndunge Mutua and Annastacia Kavindu Mutua.</h1>'
 if __name__ == '__main__':
     app.run(debug=True)
 

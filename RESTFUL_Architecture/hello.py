2d2237a11bba3c3dfd336854ab5c7e0b053bcc28
marky376 <mmutuamark@gmail.com>
2024-04-29 13:35:16 +0300

    restful api architecture

--- RESTFUL_Architecture/hello.py
+++ RESTFUL_Architecture/hello.py
@@ -28,6 +28,20 @@ def index():
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': request.json['title'],
+            'description': request.json.get('description', ""),
+            'done': False
+            }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)

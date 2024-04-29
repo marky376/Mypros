be32f285c96b86265a3a30c60572619b6e06f59b
marky376 <mmutuamark@gmail.com>
2024-04-29 13:26:42 +0300

    restful api architecture

--- RESTFUL_Architecture/hello.py
+++ RESTFUL_Architecture/hello.py
@@ -26,6 +26,10 @@ def index():
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
     app.run(debug=True)
 

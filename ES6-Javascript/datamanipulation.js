b60089ed53fadda30a2c9458777d2689370aded9
marky376 <mmutuamark@gmail.com>
2024-07-07 10:41:10 +0300

    ES6 data manipulation Array methods

---
+++ ES6-Javascript/datamanipulation.js
@@ -0,0 +1,23 @@
+const years = [];
+years.push('mark', 'chelsea', 'stacy', 'tracy', 'gift', 'kristian', 'zaza');
+years[8] = "Tina";
+years.length = 20;
+console.log(years);
+console.log(years[8]);
+console.log(Object.keys(years));
+console.log(years.length);
+
+
+
+
+years.push('mark', 'chelsea', 'stacy', 'tracy', 'gift', 'kristian', 'zaza');
+years[8] = "Tina";
+years.length = 20;
+console.log(years);
+years.length = 2;
+console.log(years);
+console.log(years[8]);
+console.log(Object.keys(years));
+console.log(years.length);
+
+// Array Methods
